437cfe8e8cb739c43b347f8831f94124538e78c6
Nicole Tsingas <nicolecass27@gmail.com>
2023-08-01 12:46:54 -0300

    Feito base de estilização da tela dos menus e navegação por rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginModule } from './components/login/login.module';
+import { MenuModule } from './components/menu/menu.module';
 
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./components/login/login.module').then( m => LoginModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./components/menu/menu.module').then( m => MenuModule)
   }
 ];
 
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  login(){
+    return console.log('cliquei no botao');
+  }
+  
 }
