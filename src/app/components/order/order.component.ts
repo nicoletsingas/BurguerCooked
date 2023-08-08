0a623572863f941a002d71ac316f6f5722776c4c
Nicole Tsingas <nicolecass27@gmail.com>
2023-08-08 11:50:27 -0300

    criado estrutura base do componente order e base do estilo

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-order',
@@ -6,9 +7,5 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent {
-  @Output() closeOrder = new EventEmitter<void>();
 
-  onCloseClick() {
-    this.closeOrder.emit();
-  }
 }
