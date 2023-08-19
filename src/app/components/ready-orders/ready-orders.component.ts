5740ad2719c0c605cfaf4f897d2af08622e9d782
Nicole Tsingas <nicolecass27@gmail.com>
2023-08-19 12:25:55 -0300

    criado requisição para buscar os pedidos completos e estilizado componente.

--- src/app/components/ready-orders/ready-orders.component.ts
+++ src/app/components/ready-orders/ready-orders.component.ts
@@ -1,19 +1,54 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+interface Order {
+  client: string;
+  table: string;
+  waiter: string;
+  status: string;
+  products: [];
+}
 
 @Component({
   selector: 'app-ready-orders',
   templateUrl: './ready-orders.component.html',
   styleUrls: ['./ready-orders.component.css']
 })
-export class ReadyOrdersComponent {
+export class ReadyOrdersComponent implements OnInit {
 
-  @Input() completedOrders: any[] = [];
   @Output() closeOrder = new EventEmitter<boolean>();
 
+  completedOrders: any[] = [];
+
+  constructor(
+    private http: HttpClient
+  ){}
+
+  ngOnInit(): void {
+    this.getCompletedOrders();
+  }
 
   onCloseClick() {
     this.closeOrder.emit(false);
   }
 
+  getCompletedOrders(){
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      const headers = new HttpHeaders({
+        Authorization: `Bearer ${token}`
+      });
+
+      this.http.get<any>('http://localhost:8080/orders', { headers }).subscribe((response) => {
+        this.completedOrders = response.filter((order: Order) => order.status === 'completed');
+      },
+      (error) => {
+        console.error('Erro ao buscar pedidos concluídos', error);
+      });
+    } else {
+      console.error('Token de autenticação não encontrado.');
+    }
+  }
 
 }
