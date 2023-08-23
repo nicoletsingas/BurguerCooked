7a735267de511f375f455af1b60bba14add381fc
Nicole Tsingas <nicolecass27@gmail.com>
2023-08-23 15:53:50 -0300

    Feito estilização do componente admin-products

--- src/app/components/admin-products/admin-products.component.ts
+++ src/app/components/admin-products/admin-products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -7,14 +7,34 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent {
+export class AdminProductsComponent implements OnInit {
 
   selectedView: string = 'list';
+  products: any [] = [];
 
   constructor(
     private http: HttpClient,
     private authService: AuthService,
   ) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+      const apiUrlProducts = 'http://localhost:8080/products';
+      this.http.get<any>(apiUrlProducts, { headers }).subscribe((response) => {
+        this.products = response;
+      },
+      (error) => {
+        console.error('Erro ao obter produtos da API:', error);
+      });
+    }
+  }
 
 
 }
